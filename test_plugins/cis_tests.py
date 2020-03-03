8772c1446c4bad7967ba94782f5de5b974032f61
Daniel Sánchez Fábregas <daniel.sanchez.fabregas@xunta.gal>
2020-03-03 14:25:41 +0100

    ✨ Add check 3.6.5 ￼

--- test_plugins/cis_tests.py
+++ test_plugins/cis_tests.py
@@ -10,8 +10,6 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-from crypt import crypt
-
 
 def cis_search_words(string, words):
     """Test if given words compose a string.
@@ -51,6 +49,48 @@ def good_rsyslog_perms(grep_stdout_lines):
     return result
 
 
+def iptables_contains_ports(iptables_stdout_lines, open_ports):
+    """
+        Auxiliary function for check 3.6.5
+
+        Parses the output of:
+        iptables -L -v -n
+        and checks that any ports that have been opened on non-loopback
+        addresses need firewall rules to govern traffic.
+    """
+
+    def parse_multiport(ports_to_parse):
+        # Example: 1500:1501,1552:1553,1581
+        ports_parsed = []
+        chunks = ports_to_parse.split(',')
+        for chunk in chunks:
+            if ':' in chunk:
+                begin, end = map(int,chunk.split(':'))
+                ports_parsed.extend(range(begin, end + 1))
+            else:
+                ports_parsed.append(int(chunk))
+        return ports_parsed
+
+    open_port_set = set()
+    for port in open_ports:
+        if (port["address"] == '::1' or port["address"].startswith('127.')):
+            continue  # Exclude loopback
+        open_port_set |= set([(port["protocol"], port["port"])])
+
+    iptables_port_set = set()
+    for line in iptables_stdout_lines:
+        if ('dpt' in line or 'dports' in line):
+            split_line = line.split()
+            protocol = split_line[3]
+            if 'dpt' in line:
+                port = int(split_line[-1].split(':')[1])
+                iptables_port_set |= set([(protocol, port)])
+            else:
+                ports = parse_multiport(split_line[-1])
+                for port in ports:
+                    iptables_port_set |= set([(protocol, port)])
+    return (open_port_set - iptables_port_set) == set()
+
 class TestModule:
     """ Ansible tests """
 
@@ -58,4 +98,5 @@ class TestModule:
         return {
             "cis_search_words": cis_search_words,
             'good_rsyslog_perms': good_rsyslog_perms,
+            'iptables_contains_ports': iptables_contains_ports,
             }
