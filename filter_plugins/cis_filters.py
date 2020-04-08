abae4ebf1210bff70a8e58c1ed98d347b49704fa
Daniel Sánchez Fábregas <daniel.sanchez.fabregas@xunta.gal>
2020-04-08 13:55:45 +0200

    ✨ Add check 5.4.5

---
+++ filter_plugins/cis_filters.py
@@ -0,0 +1,30 @@
+# Make coding more python3-ish
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+def cis_script_variable_value(variable_declarations):
+    """Parses script variable declaration lines and returns value
+
+    Args:
+        variable_declarations (string): string with variable declaration lines
+            formated VAR=VALUE
+
+    Returns:
+        string: last value declared
+    """
+
+    if variable_declarations:
+        return variable_declarations.split('\n')[-1].split('=')[1]
+    else:
+        return ""
+
+
+class FilterModule(object):
+    """Ansible win_domain_ous filters."""
+
+    def filters(self):
+        return {
+            "cis_script_variable_value": cis_script_variable_value,
+        }
